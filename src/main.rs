73e5571bf303f76af4df32d7248e18808b7c88ff
Layton Washburn <layton.washburn1659@gmail.com>
2025-01-26 15:17:25 -0700

    Saving work

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 ////
 //// Demo project found here
 ////
+//// 
+/// 
+
+mod player;
+mod platforms;
+mod  animation;
+use platforms::PlatformsPlugin;
+use player::PlayerPlugin;
+use animation::AnimationPlugin;
 use bevy::prelude::*;
 use bevy::window::WindowResolution;
 use bevy_rapier2d::prelude::RigidBody;
@@ -8,30 +17,21 @@ use bevy_rapier2d::prelude::Collider;
 use bevy_rapier2d::prelude::RapierPhysicsPlugin;
 use bevy_rapier2d::prelude::NoUserData;
 use bevy_rapier2d::prelude::RapierDebugRenderPlugin;
-use bevy_rapier2d::prelude::KinematicCharacterController;
-use bevy_rapier2d::prelude::KinematicCharacterControllerOutput;
-use bevy::input::ButtonInput;
-use bevy::render::render_resource::{AsBindGroup, ShaderRef};
-use bevy::sprite::Material2d;
-use bevy::sprite::AlphaMode2d;
 
-const WINDOW_WIDTH: f32 = 1024.0;
-const WINDOW_HEIGHT: f32 = 720.0;
+pub const WINDOW_WIDTH: f32 = 1024.0;
+pub const WINDOW_HEIGHT: f32 = 720.0;
 
-const WINDOW_BOTTOM_Y: f32 = WINDOW_HEIGHT / -2.0;
-const WINDOW_LEFT_X: f32 = WINDOW_WIDTH / -2.0;
+pub const WINDOW_BOTTOM_Y: f32 = WINDOW_HEIGHT / -2.0;
+pub const WINDOW_LEFT_X: f32 = WINDOW_WIDTH / -2.0;
 
-const COLOR_BACKGROUND: Color = Color::srgb(0.29, 0.31, 0.41);
-const COLOR_PLATFORM: Color = Color::srgb(0.13, 0.13, 0.23);
-const COLOR_PLAYER: Color = Color::srgb(0.60, 0.55, 0.60);
-const COLOR_FLOOR: Color = Color::srgb(0.45, 0.55, 0.66);
+pub const COLOR_BACKGROUND: Color = Color::srgb(0.29, 0.31, 0.41);
+pub const COLOR_FLOOR: Color = Color::srgb(0.45, 0.55, 0.66);
 
-const PLAYER_VELOCITY_X: f32 = 400.0;
-const PLAYER_VELOCITY_Y: f32 = 850.0;
+pub const PLAYER_VELOCITY_X: f32 = 400.0;
+pub const PLAYER_VELOCITY_Y: f32 = 850.0;
 
-const FLOOR_THICKNESS: f32 = 10.0;
-
-const MAX_JUMP_HEIGHT: f32 = 230.0;
+pub const FLOOR_THICKNESS: f32 = 10.0;
+pub const MAX_JUMP_HEIGHT: f32 = 230.0;
 
 fn main() {
     App::new()
@@ -47,46 +47,18 @@ fn main() {
         }))
         .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(200.0)) // Physics plugin
         .add_plugins(RapierDebugRenderPlugin::default()) // Debug plugin
+        .add_plugins(PlatformsPlugin)
+        .add_plugins(PlayerPlugin) // new plugin added
+        .add_plugins(AnimationPlugin)
         .add_systems(Startup, setup)
-        .add_systems(Update, (movement, jump, rise, fall)) // new system added
         .run();
 }
 
-fn setup(mut commands: Commands,
-         mut meshes: ResMut<Assets<Mesh>>,
-         mut materials: ResMut<Assets<ColorMaterial>>,
-        asset_server: Res<AssetServer>,
-        ) {
-
-    let r_spartan: Handle<Image> = asset_server.load("spartan_small.png");
-
-    // Plateforms
-    commands.spawn(PlatformBundle::new(-100.0, Vec3::new(75.0, 200.0, 1.0)));
-    commands.spawn(PlatformBundle::new(100.0, Vec3::new(50.0, 350.0, 1.0)));
-    commands.spawn(PlatformBundle::new(350.0, Vec3::new(150.0, 250.0, 1.0)));
+fn setup(mut commands: Commands,) 
+{
 
     // Camera
     commands.spawn(Camera2d::default());
-
-
-    // Ball
-    commands.spawn((
-        Mesh2d(meshes.add(Circle::default()).into()),
-        MeshMaterial2d(materials.add(ColorMaterial {
-            color: COLOR_PLAYER,
-            alpha_mode: AlphaMode2d::default(),
-            texture: Some(r_spartan),
-        })), // MeshMaterial2d(materials.add(ColorMaterial::from(COLOR_PLAYER)))
-        Transform {
-                translation: Vec3::new(WINDOW_LEFT_X + 50.0, WINDOW_BOTTOM_Y + 30.0, 0.0),
-                scale: Vec3::new(30.0, 30.0, 1.0),
-                ..Default::default()
-        }
-    ))
-    
-    .insert(RigidBody::KinematicPositionBased)
-    .insert(Collider::ball(0.5))
-    .insert(KinematicCharacterController::default());
 
     // Floor
     commands
@@ -106,138 +78,3 @@ fn setup(mut commands: Commands,
 
 }
 
-#[derive(AsBindGroup, Debug, Clone, Asset, TypePath)]
-pub struct CustomMaterial {
-    // Uniform bindings must implement `ShaderType`, which will be used to convert the value to
-    // its shader-compatible equivalent. Most core math types already implement `ShaderType`.
-    #[uniform(0)]
-    color: LinearRgba,
-    // Images can be bound as textures in shaders. If the Image's sampler is also needed, just
-    // add the sampler attribute with a different binding index.
-    #[texture(1)]
-    #[sampler(2)]
-    color_texture: Handle<Image>,
-}
-
-// All functions on `Material2d` have default impls. You only need to implement the
-// functions that are relevant for your material.
-impl Material2d for CustomMaterial {
-    fn fragment_shader() -> ShaderRef {
-        "shaders/custom_material.wgsl".into()
-    }
-}
-
-#[derive(Bundle)]
-struct PlatformBundle {
-    sprite: Sprite,
-    transform: Transform,
-    body: RigidBody,
-    collider: Collider
-}
-
-impl PlatformBundle {
-    fn new(x: f32, scale: Vec3) -> Self {
-        Self {
-            sprite: Sprite {
-                color: COLOR_PLATFORM,
-                ..Default::default()
-            },
-            transform : Transform {
-                translation: Vec3::new(x, WINDOW_BOTTOM_Y + (scale.y / 2.0), 0.0),
-                    scale,
-                    ..Default::default()
-            },
-            body: RigidBody::Fixed,
-            collider: Collider::cuboid(0.5, 0.5),
-        }
-    }
-}
-
-fn movement(
-    input: Res<ButtonInput<KeyCode>>,
-    time: Res<Time>,
-    mut query: Query<&mut KinematicCharacterController>,
-) {
-    let mut player = query.single_mut();
-
-    let mut movement = 0.0;
-
-    if input.pressed(KeyCode::ArrowRight) {
-        movement += time.delta_secs() * PLAYER_VELOCITY_X;
-    }
-
-    if input.pressed(KeyCode::ArrowLeft) {
-        movement += time.delta_secs() * PLAYER_VELOCITY_X * -1.0;
-    }
-
-    match player.translation {
-        Some(vec) => player.translation = Some(Vec2::new(movement, vec.y)), // update if it already exists
-        None => player.translation = Some(Vec2::new(movement, 0.0)),
-    }
-}
-
-
-#[derive(Component)]
-struct Jump(f32);
-
-fn jump(
-    input: Res<ButtonInput<KeyCode>>,
-    mut commands: Commands,
-    query: Query<
-        (Entity, &KinematicCharacterControllerOutput),
-        (With<KinematicCharacterController>, Without<Jump>),
-    >,
-) {
-    if query.is_empty() {
-        return;
-    }
-
-    let (player, output) = query.single();
-
-    if input.pressed(KeyCode::ArrowUp) && output.grounded {
-        commands.entity(player).insert(Jump(0.0));
-    }
-}
-
-fn rise(
-    mut commands: Commands,
-    time: Res<Time>,
-    mut query: Query<(Entity, &mut KinematicCharacterController, &mut Jump)>,
-) {
-    if query.is_empty() {
-        return;
-    }
-
-    let (entity, mut player, mut jump) = query.single_mut();
-
-    let mut movement = time.delta().as_secs_f32() * PLAYER_VELOCITY_Y;
-
-    if movement + jump.0 >= MAX_JUMP_HEIGHT {
-        movement = MAX_JUMP_HEIGHT - jump.0;
-        commands.entity(entity).remove::<Jump>();
-    }
-
-    jump.0 += movement;
-
-    match player.translation {
-        Some(vec) => player.translation = Some(Vec2::new(vec.x, movement)),
-        None => player.translation = Some(Vec2::new(0.0, movement)),
-    }
-}
-
-
-fn fall(time: Res<Time>, mut query: Query<&mut KinematicCharacterController, Without<Jump>>) {
-    if query.is_empty() {
-        return;
-    }
-
-    let mut player = query.single_mut();
-
-    // I am using two-thirds of the Y-velocity since I want the character to fall slower than it rises
-    let movement = time.delta().as_secs_f32() * (PLAYER_VELOCITY_Y / 1.5) * -1.0;
-
-    match player.translation {
-        Some(vec) => player.translation = Some(Vec2::new(vec.x, movement)),
-        None => player.translation = Some(Vec2::new(0.0, movement)),
-    }
-}
---
+++ src/player.rs
@@ -0,0 +1,224 @@
+pub struct PlayerPlugin;
+
+use std::time::Duration;
+
+use crate::animation::{AnimationIndices, animate_sprite, apply_idle_sprite, AnimationTimer, Animation};
+use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
+use bevy::sprite::Material2d;
+use bevy::sprite::TextureAtlas;
+use bevy_rapier2d::prelude::KinematicCharacterController;
+use bevy_rapier2d::prelude::KinematicCharacterControllerOutput;
+use bevy::input::ButtonInput;
+use bevy::render::render_resource::{AsBindGroup, ShaderRef};
+use crate::MAX_JUMP_HEIGHT;
+use crate::WINDOW_LEFT_X;
+use crate::WINDOW_HEIGHT;
+use crate::PLAYER_VELOCITY_X;
+use crate::PLAYER_VELOCITY_Y;
+
+// use crate::WINDOW_WIDTH;
+const SPRITESHEET_COLS: u32 = 7;
+const SPRITESHEET_ROWS: u32 = 8;
+const SPRITE_TILE_WIDTH: u32 = 128;
+const SPRITE_TILE_HEIGHT: u32 =256;
+const SPRITE_IDX_STAND: usize = 28;
+const SPRITE_IDX_WALKING: &[usize] = &[7, 0];
+const CYCLE_DELAY: Duration = Duration::from_millis(70);
+
+// const COLOR_PLAYER: Color = Color::srgb(0.60, 0.55, 0.60);
+pub const WINDOW_BOTTOM_Y: f32 = WINDOW_HEIGHT / -2.0;
+
+
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, setup)
+        .add_systems(Update, movement)
+        .add_systems(Update, jump)
+        .add_systems(Update, rise)
+        .add_systems(Update, fall)
+        .add_systems(Update,animate_sprite)
+        .add_systems(Update,apply_idle_sprite);
+    }
+}
+
+
+fn setup(
+    mut commands: Commands,
+    mut atlases: ResMut<Assets<TextureAtlasLayout>>,
+    server: Res<AssetServer>,
+) 
+{
+    let image_handle: Handle<Image> = server.load("spritesheets/spritesheet_players.png");
+    let layout = TextureAtlasLayout::from_grid(UVec2::new(SPRITE_TILE_WIDTH, SPRITE_TILE_HEIGHT), 
+                                                                            SPRITESHEET_COLS, 
+                                                                                     SPRITESHEET_ROWS, None, None);
+
+    let layout_handle = atlases.add(layout);
+    let animation_indices = AnimationIndices { first: 7, last: 0 };
+    let texture_atlas:TextureAtlas = TextureAtlas{layout:layout_handle, 
+                                                  index:animation_indices.first};
+
+    commands.spawn((    
+        Sprite {
+            texture_atlas: Some(texture_atlas.clone()),
+            image: image_handle.clone(),
+            custom_size: Some(Vec2::new(SPRITE_TILE_WIDTH as f32 * 0.01, SPRITE_TILE_HEIGHT as f32 * 0.01)),  // Match sprite size to atlas sprite size
+            ..Default::default()
+        },
+        Transform {
+                translation: Vec3::new(WINDOW_LEFT_X + 50.0, WINDOW_BOTTOM_Y + 50.0, 0.0),
+                scale: Vec3::new(30.0, 30.0, 1.0),
+                ..Default::default()
+        },
+        animation_indices,
+        AnimationTimer(Timer::new(Duration::new(1, 0), TimerMode::Repeating))
+    ))
+    .insert(RigidBody::KinematicPositionBased)
+    .insert(Collider::cuboid(    // collider updated
+        (SPRITE_TILE_WIDTH as f32 * 0.01) / 2.0,
+        (SPRITE_TILE_HEIGHT as f32 * 0.01) / 2.0,
+        
+    ))
+    .insert(KinematicCharacterController::default());
+}
+
+
+
+
+
+#[derive(AsBindGroup, Debug, Clone, Asset, TypePath)]
+pub struct CustomMaterial {
+    // Uniform bindings must implement `ShaderType`, which will be used to convert the value to
+    // its shader-compatible equivalent. Most core math types already implement `ShaderType`.
+    #[uniform(0)]
+    color: LinearRgba,
+    // Images can be bound as textures in shaders. If the Image's sampler is also needed, just
+    // add the sampler attribute with a different binding index.
+    #[texture(1)]
+    #[sampler(2)]
+    color_texture: Handle<Image>,
+}
+
+// All functions on `Material2d` have default impls. You only need to implement the
+// functions that are relevant for your material.
+impl Material2d for CustomMaterial {
+    fn fragment_shader() -> ShaderRef {
+        "shaders/custom_material.wgsl".into()
+    }
+}
+
+fn movement(
+    input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+    mut query: Query<&mut KinematicCharacterController>,
+) {
+    let mut player = query.single_mut();
+
+    let mut movement = 0.0;
+
+    if input.pressed(KeyCode::ArrowRight) {
+        movement += time.delta_secs() * PLAYER_VELOCITY_X;
+    }
+
+    if input.pressed(KeyCode::ArrowLeft) {
+        movement += time.delta_secs() * PLAYER_VELOCITY_X * -1.0;
+    }
+
+    match player.translation {
+        Some(vec) => player.translation = Some(Vec2::new(movement, vec.y)), // update if it already exists
+        None => player.translation = Some(Vec2::new(movement, 0.0)),
+    }
+}
+
+
+#[derive(Component)]
+struct Jump(f32);
+
+fn jump(
+    input: Res<ButtonInput<KeyCode>>,
+    mut commands: Commands,
+    query: Query<
+        (Entity, &KinematicCharacterControllerOutput),
+        (With<KinematicCharacterController>, Without<Jump>),
+    >,
+) {
+    if query.is_empty() {
+        return;
+    }
+
+    let (player, output) = query.single();
+
+    if input.pressed(KeyCode::ArrowUp) && output.grounded {
+        commands.entity(player).insert(Jump(0.0));
+    }
+}
+
+fn rise(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut query: Query<(Entity, &mut KinematicCharacterController, &mut Jump)>,
+) {
+    if query.is_empty() {
+        return;
+    }
+
+    let (entity, mut player, mut jump) = query.single_mut();
+
+    let mut movement = time.delta().as_secs_f32() * PLAYER_VELOCITY_Y;
+
+    if movement + jump.0 >= MAX_JUMP_HEIGHT {
+        movement = MAX_JUMP_HEIGHT - jump.0;
+        commands.entity(entity).remove::<Jump>();
+    }
+
+    jump.0 += movement;
+
+    match player.translation {
+        Some(vec) => player.translation = Some(Vec2::new(vec.x, movement)),
+        None => player.translation = Some(Vec2::new(0.0, movement)),
+    }
+}
+
+
+fn fall(time: Res<Time>, mut query: Query<&mut KinematicCharacterController, Without<Jump>>) {
+    if query.is_empty() {
+        return;
+    }
+
+    let mut player = query.single_mut();
+
+    // I am using two-thirds of the Y-velocity since I want the character to fall slower than it rises
+    let movement = time.delta().as_secs_f32() * (PLAYER_VELOCITY_Y / 1.5) * -1.0;
+
+    match player.translation {
+        Some(vec) => player.translation = Some(Vec2::new(vec.x, movement)),
+        None => player.translation = Some(Vec2::new(0.0, movement)),
+    }
+}
+
+
+fn apply_movement_animation(
+    mut commands: Commands,
+    query: Query<(Entity, &KinematicCharacterControllerOutput), Without<Animation>>,
+) {
+    if query.is_empty() {
+        return;
+    }
+
+    let (player, output) = query.single();
+    if output.desired_translation.x != 0.0 && output.grounded {
+        commands
+            .entity(player)
+            .insert(Animation::new(SPRITE_IDX_WALKING, CYCLE_DELAY));
+    }
+}
+
+
+
+
+
+
+
+
+
